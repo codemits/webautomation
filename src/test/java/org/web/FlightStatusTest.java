625752b9a5a09978de7853e02e82346da099d846
ankitpundhir <40563166+ankitpundhir@users.noreply.github.com>
2024-03-18 00:50:40 +0530

    Update FlightStatusTest.java

--- src/test/java/org/web/FlightStatusTest.java
+++ src/test/java/org/web/FlightStatusTest.java
@@ -20,7 +20,7 @@ public class FlightStatusTest extends TestBase {
         initialization();
         flightStatus = new FlightStatus();
         flightStatus.acceptCookie();
-        percy = new Percy(driver);
+        chome = new chrome(driver);
     }
 
     @Test
